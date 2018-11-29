7b86e2c13fc57fe27fc6be9e50b00ab07c4e8862
Jordan Burns <jordanphillipburns@berkeley.edu>
2018-11-29 15:50:00 -0800

    first draft of proj1 gold finished

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -7,17 +7,40 @@ public class TestArrayDequeGold {
     public void Tester() {
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> happy1 = new ArrayDequeSolution<>();
+        String operations = "";
 
         for (int i = 0; i < 10; i += 1) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
-            if (numberBetweenZeroAndOne < 0.5) {
+            if (numberBetweenZeroAndOne <= 0.5) {
                 sad1.addFirst(i);
                 happy1.addFirst(i);
-                assertEquals(sad1.get(0), happy1.get(0));
+                Integer expected = happy1.get(0);
+                Integer actual = sad1.get(0);
+                operations += "addFirst(" + i + ")\n";
+                assertEquals(operations, expected, actual);
             } else {
                 sad1.addLast(i);
                 happy1.addLast(i);
-                assertEquals(sad1.get(sad1.size() - 1), happy1.get(happy1.size() - 1));
+                Integer expected = happy1.get(happy1.size() - 1);
+                Integer actual = sad1.get(sad1.size() - 1);
+                operations += "addLast(" + i + ")\n";
+                assertEquals(operations, expected, actual);
+            }
+        }
+        for (int i = 0; i < 10; i+= 1) {
+            double numberbw0and1 = StdRandom.uniform();
+            if (numberbw0and1 <= 0.5) {
+                Integer actual = sad1.removeFirst();
+                Integer expected = happy1.removeFirst();
+                operations += "removeFirst()\n";
+                assertEquals(operations, expected, actual);
+
+            }
+            else {
+                Integer actual = sad1.removeLast();
+                Integer expected = happy1.removeLast();
+                operations += "removeLast()\n";
+                assertEquals(operations, expected, actual);
             }
         }
     }
